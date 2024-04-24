e57d470543a30727645bd1ce9f70e3768d7ec2b7
angelo <aperezr@unsa.edu.pe>
2024-04-24 08:21:19 -0500

    Correcion envolver metodos repetitivos en onResetClose/esperar el contexto para mejorar busqueda

--- src/iu/componentes/ModalBusqueda.jsx
+++ src/iu/componentes/ModalBusqueda.jsx
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import { IoSearchSharp } from "react-icons/io5";
 import { useState } from "react";
 import axios from "axios";
-import { Link,useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 export const ModalBusqueda = ({ show, handleClose }) => {
   const API = import.meta.env.VITE_API_BACK;
   const [productosEncontrados, setProductosEncontrados] = useState([]);
@@ -23,7 +23,6 @@ export const ModalBusqueda = ({ show, handleClose }) => {
       setBusquedaUrl("");
     }
   };
-
   const handleOnChange = (e) => {
     if (e.target.value.length <= 0) {
       setProductosEncontrados([]);
@@ -31,21 +30,18 @@ export const ModalBusqueda = ({ show, handleClose }) => {
     }
     getProductos(e.target.value);
   };
+  const onResetClose = () => {
+    handleClose();
+    setBusquedaUrl(API);
+    setProductosEncontrados([]);
+  };
   return (
-    <Modal
-      show={show}
-      onHide={() => {
-        handleClose();
-        setBusquedaUrl(API);
-        setProductosEncontrados([]);
-      }}
-      
-    >
+    <Modal show={show} onHide={onResetClose}>
       <Modal.Header closeButton className="bg-modal-color-header">
         <Modal.Title>Buscador</Modal.Title>
       </Modal.Header>
       <Modal.Body className="bg-modal-color-body">
-        <Form className="d-flex" >
+        <Form className="d-flex">
           <Form.Control
             type="search"
             placeholder="Search"
@@ -57,38 +53,55 @@ export const ModalBusqueda = ({ show, handleClose }) => {
             <IoSearchSharp />
           </Button>
         </Form>
-      <hr className="mx-3" />
-      {productosEncontrados.length <= 0 ? (
-        <p className="text-center " style={{fontSize:"14px"}}>Buscar Producto</p>
-      ) : (
-        <>
-          {productosEncontrados.map((producto) => (
-            <div key={producto._id} className="d-flex my-3 p-1 card-producto rounded-3">
-              <section style={{ width: "80px" }}>
-                <img
-                  src={producto.imagen}
-                  alt={producto.nombre}
-                  style={{ width: "100%", height: "100%" }}
-                />
-              </section>
-              <section className="ps-3">
-                <h5>{producto.nombre}</h5>
-                <h6>${producto.precio}</h6>
-                <div>
-                  <button className="rounded-5 m-2 enlace-producto" onClick={()=>{
-                    handleClose()
-                    navigate(`/detalles/${producto._id}`)
-                  }}>Ver producto</button>
-                </div>
-              </section>
-            </div>
-          ))}
-          <Link to={`/busqueda/${busquedaUrl}`} className="text-center d-block bg-danger link-light">
-            Ver más
-          </Link>
-        </>
-      )}
-      </Modal.Body >
+        <hr className="mx-3" />
+        {productosEncontrados.length <= 0 ? (
+          <p className="text-center " style={{ fontSize: "14px" }}>
+            Buscar Producto
+          </p>
+        ) : (
+          <>
+            {productosEncontrados.map((producto) => (
+              <div
+                key={producto._id}
+                className="d-flex my-3 p-1 card-producto rounded-3"
+              >
+                <section style={{ width: "80px" }}>
+                  <img
+                    src={producto.imagen}
+                    alt={producto.nombre}
+                    style={{ width: "100%", height: "100%" }}
+                  />
+                </section>
+                <section className="ps-3">
+                  <h5>{producto.nombre}</h5>
+                  <h6>${producto.precio}</h6>
+                  <div>
+                    <button
+                      className="rounded-5 m-2 enlace-producto"
+                      onClick={() => {
+                        handleClose();
+                        navigate(`/detalles/${producto._id}`);
+                      }}
+                    >
+                      Ver producto
+                    </button>
+                  </div>
+                </section>
+              </div>
+            ))}
+            <Link
+              to={`/busqueda/${busquedaUrl}`}
+              className="text-center d-block bg-danger link-light"
+              onClick={()=>{
+                onResetClose();
+                navigate(`/busqueda/${busquedaUrl}`);
+              }}
+            >
+              Ver más
+            </Link>
+          </>
+        )}
+      </Modal.Body>
     </Modal>
   );
 };
