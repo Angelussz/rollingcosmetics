4917fb916e51815ad07bcbf3a64d3a558246b4ee
Facundo <facupalacios21@gmail.com>
2024-04-28 22:55:11 -0300

    se arreglaron los validadores y se integro con el back

--- src/components/pages/Registro.jsx
+++ src/components/pages/Registro.jsx
@@ -1,23 +1,53 @@
 import React, { useState } from 'react';
-import { Form, Button } from 'react-bootstrap';
-import "./estilos/registro.css"
+import { Form, Button, Modal } from 'react-bootstrap';
+import "./estilos/registro.css";
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 const Registro = () => {
     const [nombre, setNombre] = useState('');
     const [apellido, setApellido] = useState('');
     const [email, setEmail] = useState('');
     const [clave, setClave] = useState('');
-    const [rol, setRol] = useState('Usuario');
     const [nombreError, setNombreError] = useState('');
     const [apellidoError, setApellidoError] = useState('');
-    
+    const [emailError, setEmailError] = useState('');
+    const [claveError, setClaveError] = useState('');
+    const [showSuccessModal, setShowSuccessModal] = useState(false);
+    const [showErrorModal, setShowErrorModal] = useState(false);
+
+    const navigate = useNavigate();
 
     const API = import.meta.env.VITE_API;
 
     const handleRegistro = (e) => {
         e.preventDefault();
-        // Acá iría conex con el back je
-        console.log({ nombre, apellido, email, clave, rol });
+        enviarUsuarios();
+    };
+
+    const enviarUsuarios = async () => {
+        try {
+            const cuerpoEnviar = { nombre, apellido, email, clave };
+            const respuesta = await axios.post(`${API}/usuarios`, cuerpoEnviar);
+            if (respuesta.status === 201) {
+                setShowSuccessModal(true);
+                //Formatear formulario
+            } else {
+                setShowErrorModal(true);
+            }
+        } catch (error) {
+            console.error(error);
+            setShowErrorModal(true);
+        }
+    };
+
+    const handleCloseSuccessModal = () => {
+        setShowSuccessModal(false);
+        navigate("/");
+    };
+
+    const handleCloseErrorModal = () => {
+        setShowErrorModal(false);
     };
 
     const validateNombre = (nombre) => {
@@ -49,6 +79,26 @@ const Registro = () => {
             setApellidoError('');
         }
     };
+    
+    const handleEmailChange = (e) => {
+        const newValue = e.target.value;
+        setEmail(newValue);
+        if (!validateEmail(newValue)) {
+            setEmailError('Por favor ingresa un email válido.');
+        } else {
+            setEmailError('');
+        }
+    };
+
+    const handleClaveChange = (e) => {
+        const newValue = e.target.value;
+        setClave(newValue);
+        if (!validateClave(newValue)) {
+            setClaveError('La clave debe contener al menos una mayúscula, un número, un carácter especial (!*@#$%^&+=) y tener entre 6 y 20 caracteres.');
+        } else {
+            setClaveError('');
+        }
+    };
 
     const validateEmail = (email) => {
         const re =
@@ -70,83 +120,101 @@ const Registro = () => {
                     <p>Coloca tus datos y síguenos en Nuestras Redes para estar al tanto de nuestras Promociones</p>
                 </div>
             </div>
-            <div className='container fondoSecundarioRegistro my-5 py-2' style={{ borderRadius: "10px" }}>
-                <Form onSubmit={handleRegistro}>
-                    <Form.Group controlId="formNombre">
-                        <Form.Label>Nombre</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Nombre"
-                            value={nombre}
-                            onChange={handleNombreChange}
-                            required
-                            isInvalid={nombreError !== ''}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {nombreError}
-                        </Form.Control.Feedback>
-                    </Form.Group>
+            <div className='orden d-flex container'>
+                <div className='contenedorRegistro fondoSecundarioRegistro my-5 py-2 container' style={{ borderRadius: "10px" }}>
+                    <Form onSubmit={handleRegistro}>
+                        <Form.Group controlId="formNombre">
+                            <Form.Label>Nombre</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Nombre"
+                                value={nombre}
+                                onChange={handleNombreChange}
+                                required
+                                isInvalid={nombreError !== ''}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {nombreError}
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-                    <Form.Group controlId="formApellido">
-                        <Form.Label>Apellido</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Apellido"
-                            value={apellido}
-                            onChange={handleApellidoChange}
-                            required
-                            isInvalid={apellidoError !== ''}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            {apellidoError}
-                        </Form.Control.Feedback>
-                    </Form.Group>
+                        <Form.Group controlId="formApellido">
+                            <Form.Label>Apellido</Form.Label>
+                            <Form.Control
+                                type="text"
+                                placeholder="Apellido"
+                                value={apellido}
+                                onChange={handleApellidoChange}
+                                required
+                                isInvalid={apellidoError !== ''}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {apellidoError}
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-                    <Form.Group controlId="formEmail">
-                        <Form.Label>Email</Form.Label>
-                        <Form.Control
-                            type="email"
-                            placeholder="Email"
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            required isInvalid={!validateEmail(email)}
-                            minLength={7}
-                            maxLength={35}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            Por favor ingresa un email válido.
-                        </Form.Control.Feedback>
-                    </Form.Group>
+                        <Form.Group controlId="formEmail">
+                            <Form.Label>Email</Form.Label>
+                            <Form.Control
+                                type="email"
+                                placeholder="Email"
+                                value={email}
+                                onChange={handleEmailChange}
+                                required
+                                isInvalid={emailError !== ''}
+                                minLength={7}
+                                maxLength={35}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {emailError}
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-                    <Form.Group controlId="formClave">
-                        <Form.Label>Clave</Form.Label>
-                        <Form.Control
-                            type="password"
-                            placeholder="Clave"
-                            value={clave}
-                            onChange={(e) => setClave(e.target.value)}
-                            minLength={6}
-                            maxLength={20}
-                            required
-                            isInvalid={!validateClave(clave)}
-                        />
-                        <Form.Control.Feedback type="invalid">
-                            La clave debe contener al menos una mayúscula, un número, un carácter especial (!*@#$%^&+=) y tener entre 6 y 20 caracteres.
-                        </Form.Control.Feedback>
-                    </Form.Group>
+                        <Form.Group controlId="formClave">
+                            <Form.Label>Contraseña</Form.Label>
+                            <Form.Control
+                                type="password"
+                                placeholder="Clave"
+                                value={clave}
+                                onChange={handleClaveChange}
+                                minLength={6}
+                                maxLength={20}
+                                required
+                                isInvalid={claveError !== ''}
+                            />
+                            <Form.Control.Feedback type="invalid">
+                                {claveError}
+                            </Form.Control.Feedback>
+                        </Form.Group>
 
-                    <Form.Group controlId="formRol" style={{ display: "none" }}>
-                        <Form.Label>Rol</Form.Label>
-                        <Form.Control
-                            type="text"
-                            placeholder="Rol"
-                            value={rol} readOnly
-                        />
-                    </Form.Group>
-
-                    <Button variant="outline-success" size="lg" className='my-3'>Registrar</Button>
-                </Form>
+                        <Button type="submit" variant="outline-success" size="lg" className='my-3'>Registrar</Button>
+                    </Form>
+                </div>
+                <div className='contenedorRegistro'>
+                    <img className='imagenRegistro sombra' src="https://ganapromo.com/wp-content/uploads/2022/01/loreal-gana-kit.jpeg" alt="imagen de registro" />
+                </div>
             </div>
+            {/* Modales */}
+            <Modal show={showSuccessModal} onHide={handleCloseSuccessModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Éxito - Usuario creado correctamente</Modal.Title>
+                </Modal.Header>
+                <Modal.Footer>
+                    <Button variant="success" onClick={handleCloseSuccessModal}>
+                        Aceptar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            <Modal show={showErrorModal} onHide={handleCloseErrorModal}>
+                <Modal.Header closeButton>
+                    <Modal.Title>Error - Usuario con el correo ya ingresado</Modal.Title>
+                </Modal.Header>
+                <Modal.Footer>
+                    <Button variant="danger" onClick={handleCloseErrorModal}>
+                        Aceptar
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 };
