71d4b14f57823640c53ec8d562f52560b151bc5c
angelo <aperezr@unsa.edu.pe>
2024-04-30 00:30:46 -0500

    Correccion del Links para enviar a las paginas en Navbar

--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -8,18 +8,20 @@ import { AuthContext } from "../../context";
 import Col from "react-bootstrap/Col";
 import { UsuarioNavbar } from "../../iu/componentes/UsuarioNavbar";
 import { IoMdHelpCircle } from "react-icons/io";
+import { NavLink } from "react-router-dom";
 
 const Navbarr = () => {
   const [show, setShow] = useState(false);
-  const { usuarioActual, RemoverUsuario,setUsuarioActual } = useContext(AuthContext);
+  const { usuarioActual, RemoverUsuario, setUsuarioActual } =
+    useContext(AuthContext);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
-  const cerrarSesion = ()=>{
+  const cerrarSesion = () => {
     RemoverUsuario();
     setUsuarioActual(undefined);
-  }
+  };
   return (
-    <Navbar expand="lg" className="bg-danger">
+    <Navbar expand="lg" className="bg-danger" data-bs-theme="dark" >
       <Container>
         {/* <Navbar.Brand href="#home" className="d-none d-lg-block">
           Inicio
@@ -27,25 +29,49 @@ const Navbarr = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Col lg={{ order: "last" }}>
           <div className="d-flex justify-content-end align-items-center">
-            {
-              usuarioActual !== undefined ? <UsuarioNavbar usuarioActual={usuarioActual} cerrarSesion={cerrarSesion} /> : <Button variant="primary" onClick={handleShow}>
+            {usuarioActual !== undefined ? (
+              <UsuarioNavbar
+                usuarioActual={usuarioActual}
+                cerrarSesion={cerrarSesion}
+              />
+            ) : (
+              <Button variant="primary" onClick={handleShow}>
                 Iniciar Sesión
               </Button>
-            }
-          <IoMdHelpCircle style={{ fontSize: "40px" }} className="ms-3 text-light"/>
+            )}
+            <NavLink to={"/contacto"}>
+              <IoMdHelpCircle
+                style={{ fontSize: "40px" }}
+                className="ms-3 text-light"
+              />
+            </NavLink>
           </div>
         </Col>
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Inicio</Nav.Link>
-            <Nav.Link href="#home">Destacado</Nav.Link>
-            <Nav.Link href="#home">Contacto</Nav.Link>
-            <Nav.Link href="#home">Acerca de nosotros</Nav.Link>
-            {
-              usuarioActual?.rol === "Admin"?<Nav.Link href="#link">Administracion</Nav.Link>:usuarioActual?.rol === "Usuario"?<Nav.Link href="#home">Favoritos</Nav.Link>:<></>
-            }
-            
-            
+            <NavLink to="/" className={"nav-link"}>
+              Inicio
+            </NavLink>
+            <NavLink to="/destacados" className={"nav-link"}>
+              Destacado
+            </NavLink>
+            <NavLink to="/contacto" className={"nav-link"}>
+              Contacto
+            </NavLink>
+            <NavLink to="/nosotros" className={"nav-link"}>
+              Acerca de nosotros
+            </NavLink>
+            {usuarioActual?.rol === "Admin" ? (
+              <NavLink to="/administracion" className={"nav-link"}>
+                Administracion
+              </NavLink>
+            ) : usuarioActual?.rol === "Usuario" ? (
+              <NavLink to="/favoritos" className={"nav-link"}>
+                Favoritos
+              </NavLink>
+            ) : (
+              <></>
+            )}
           </Nav>
         </Navbar.Collapse>
       </Container>
